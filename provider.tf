09a236bfa5ec07da9f48d7f70a2cc685e9071b41
shambhu kumar <shambhu_kumar@shambhu.local>
2020-02-22 21:30:13 +0530

    update

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4IAE6FTPBELSQPLQ"
-  secret_key = "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"
+  access_key = "BELSQPLQ"
+  secret_key = "dM7SSkUwuOq"
 }
